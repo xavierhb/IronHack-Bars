6171ef708fd158794b5da710f8e608b40f3203a7
Xavier Hernandez <xavier@xavierhernandez.com>
2014-06-06 09:58:06 +0200

    Initial Commit

---
+++ Bars/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Bars
+//
+//  Created by Xavier Hernandez on 06/06/14.
+//  Copyright (c) 2014 ironhack. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ BarsTests/BarsTests.m
@@ -0,0 +1,34 @@
+//
+//  BarsTests.m
+//  BarsTests
+//
+//  Created by Xavier Hernandez on 06/06/14.
+//  Copyright (c) 2014 ironhack. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface BarsTests : XCTestCase
+
+@end
+
+@implementation BarsTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
